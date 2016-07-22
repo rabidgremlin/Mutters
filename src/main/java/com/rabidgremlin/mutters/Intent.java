d163f48f4012675cc4d65e958035d0033ffd14e7
rabidgremlin <jack@rabidgremlin.com>
2016-07-22 16:46:34 +1200

    Adding intent and intent matcher

--- src/main/java/com/rabidgremlin/mutters/Intent.java
+++ src/main/java/com/rabidgremlin/mutters/Intent.java
@@ -1,5 +1,38 @@
 package com.rabidgremlin.mutters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Intent {
 
+	private String name;
+	private Slots slots = new Slots();
+	private List<Utterance> utterances = new ArrayList<Utterance>();
+
+	public Intent(String name) {
+		super();
+		this.name = name;
+	}
+
+	public void addSlot(Slot slot) {
+		slots.add(slot);
+	}
+
+	public void addUtterance(Utterance utterance) {
+		utterances.add(utterance);
+	}
+
+	public UtteranceMatch matches(List<String> inputTokens) {
+
+		for (Utterance utterance:utterances)
+		{
+			UtteranceMatch match = utterance.matches(inputTokens, slots);
+			if (match.isMatched())
+			{
+				return match;
+			}
+		}
+		
+		return new UtteranceMatch(false);
+	}
 }
--- src/main/java/com/rabidgremlin/mutters/IntentMatcher.java
+++ src/main/java/com/rabidgremlin/mutters/IntentMatcher.java
@@ -1,9 +1,29 @@
 package com.rabidgremlin.mutters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntentMatcher {
-	
-	public Intent match(String utterance)
-	{
+
+	private List<Intent> intents = new ArrayList<Intent>();
+
+	public void addIntent(Intent intent) {
+		intents.add(intent);
+	}
+
+	public IntentMatch match(String utterance) {
+		return match(Utils.tokenize(utterance));
+	}
+
+	public IntentMatch match(List<String> utteranceTokens) {
+
+		for (Intent intent : intents) {
+			UtteranceMatch utteranceMatch = intent.matches(utteranceTokens);
+			if (utteranceMatch.isMatched()) {
+				return new IntentMatch(intent, utteranceMatch.getSlotMatches());
+			}
+		}
+
 		return null;
 	}
 
