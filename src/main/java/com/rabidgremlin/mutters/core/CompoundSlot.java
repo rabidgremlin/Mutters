72ea06e8572a1283bb25a7310b8f723f414caf2f
rabidgremlin <jack@rabidgremlin.com>
2016-08-15 14:43:52 +1200

    Added compound slot.

---
+++ src/main/java/com/rabidgremlin/mutters/core/CompoundSlot.java
@@ -0,0 +1,34 @@
+package com.rabidgremlin.mutters.core;
+
+public class CompoundSlot implements Slot
+{
+	private String name;
+	private Slot firstSlot;
+	private Slot secondSlot;
+
+	public CompoundSlot(String name, Slot firstSlot, Slot secondSlot)
+	{
+		this.name = name;
+		this.firstSlot = firstSlot;
+		this.secondSlot = secondSlot;
+	}
+
+	@Override
+	public SlotMatch match(String token, Context context)
+	{
+		SlotMatch match = firstSlot.match(token, context);
+		if (match == null)
+		{
+			match = secondSlot.match(token, context);
+		}
+
+		return match;
+	}
+
+	@Override
+	public String getName()
+	{
+		return name;
+	}
+
+}
